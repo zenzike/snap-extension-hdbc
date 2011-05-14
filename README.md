7d7f132c7ec5545885b0917f2fe56c6a3786d7b4
Nicolas Wu <zenzike@gmail.com>
2011-05-14 19:39:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Snap Extension for HDBC
+=======================
+
+This package provides an HDBC connection that can be used as part
+of the state in a Snap application.
+
+This API is currently a work in progress, and should not be considered stable.
---
+++ src/Snap/Extension/HDBC.hs
@@ -0,0 +1,53 @@
+{-|
+
+'Snap.Extension.HDBC' exports the 'MonadHDBC' interface which allows you to
+integerate HDBC connections with your Snap application.
+The different implementations of this interface are used for specific
+HDBC backends, such as Sqlite3, PostgreSQL, ODBC, and MySQL.
+Each of these has a different 'hdbcInitializer'.
+
+-}
+
+module Snap.Extension.HDBC
+  ( MonadHDBC ( .. )
+  , HDBCState ( HDBCState )
+  , HasHDBCState ( .. )
+  ) where
+
+import Control.Monad.Trans.Reader ( ReaderT )
+import Control.Monad.Reader       ( asks )
+
+import Database.HDBC ( ConnWrapper (..), disconnect )
+
+import Snap.Types     ( MonadSnap )
+import Snap.Extension ( SnapExtend, InitializerState(..), Initializer, mkInitializer )
+
+------------------------------------------------------------------------------
+-- | The 'MonadHDBC' type class.
+-- This provides a 'ConnWrapper' which can be used for database querying.
+class (MonadSnap m) => MonadHDBC m where
+  -- | The database connection that was established when your application was
+  -- started.
+  connWrapper :: m ConnWrapper
+
+-----------------------------------------------------------------------------
+newtype HDBCState = HDBCState
+  { _connection :: ConnWrapper
+  }
+
+-- | An application that 'HasHDBCState' is a 'MonadHDBC' whose state
+-- supports a connection supplied by HDBC.
+class HasHDBCState s where
+  getHDBCState :: s -> HDBCState
+  setHDBCState :: HDBCState-> s -> s
+
+instance HasHDBCState s => MonadHDBC (SnapExtend s) where
+  connWrapper = fmap _connection $ asks getHDBCState
+
+instance (MonadSnap m, HasHDBCState s) => MonadHDBC (ReaderT s m) where
+  connWrapper = fmap _connection $ asks getHDBCState
+
+instance InitializerState HDBCState where
+  extensionId = const "HDBC/HDBC"
+  mkCleanup   = disconnect . _connection
+  mkReload    = const $ return ()
